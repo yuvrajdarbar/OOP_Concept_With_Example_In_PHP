4dbd06c8029655ec3b9df763aa1acecc379b5ba7
Yuvraj <yuvrajdarbar2@gmail.com>
2022-12-26 08:23:59 +0530

    Class started

---
+++ Object_oriented_programming.php
@@ -0,0 +1,5 @@
+<!--   ----------* class *----------------------
+1. Class is a set of object which shares common characteristics/behavior and common properties/attributrs.
+2. class is not a real world entiry. it is just a template or blueprint or prototype from which object are created.
+3. Class does not occupy memory.
+4. Class is a group of variables of different data types and group of methods. -->
